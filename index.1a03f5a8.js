22dd76688362de91cc078e6823a98b63e3104efb
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2024-12-03 17:20:56 +0100

    Updates

---
+++ index.1a03f5a8.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.createElement("div");e.className="grid",document.body.appendChild(e),document.addEventListener("keydown",d=>{d.ctrlKey&&"KeyG"===d.code&&e.classList.toggle("grid_visible")})})(),console.log("Hello, world!");
+//# sourceMappingURL=index.1a03f5a8.js.map
