1af601a9237fb36139a4928b3d0cd5461a13a76e
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2024-12-03 20:58:25 +0100

    lecture Add babel plugin

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ import { addGrid } from './grid.js';
 addGrid();
 
 console.log(camelCase('foo-bar'));
+
+const exclaim = (string) => string + '!!!';
+const listen = (string) => 'Listen up! ' + string;
+const uppercase = (string) => string.toUpperCase();
+
+console.log('foo' |> listen(%) |> exclaim(%) |> uppercase(%));
