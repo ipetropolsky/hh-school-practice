d5ee545794ab4c9537af70fa8ea99ef836afe4b9
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2024-12-03 14:32:12 +0100

    Updates

--- index.4eb7dc73.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e=document.createElement("div");e.className="grid",document.body.appendChild(e),document.addEventListener("keydown",d=>{d.ctrlKey&&"KeyG"===d.code&&e.classList.toggle("grid_visible")})})(),console.log("foo bar");
-//# sourceMappingURL=index.4eb7dc73.js.map
