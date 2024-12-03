bc2b6b6d3b8a94f327a9a875897c4f2ca4fba8e2
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2024-12-03 15:08:21 +0100

    lecture Add css module

--- js/grid.js
+++ js/grid.js
@@ -1,11 +1,13 @@
+import styles from './grid.module.less';
+
 export const addGrid = () => {
     const grid = document.createElement('div');
-    grid.className = 'grid';
+    grid.className = styles.grid;
     document.body.appendChild(grid);
 
     document.addEventListener('keydown', (event) => {
         if (event.ctrlKey && event.code === 'KeyG') {
-            grid.classList.toggle('grid_visible');
+            grid.classList.toggle(styles.grid_visible);
             console.log([...grid.classList]);
         }
     });
