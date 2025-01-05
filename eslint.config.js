0d3b87d4e0ab0d5eab0bab0bc1d8bf8649bb6683
Vladimir Belonogov <hhatuba3@gmail.com>
2025-01-05 18:19:42 +0700

    Донастроил ESLint, сделал прекомит исполняемым

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,23 @@
-module.exports = [
-  {
-      files: ['js/**/*.js'],
-      rules: {
-          'no-const-assign': 'error',
-      },
-  },
+export default [
+    {
+        ignores: ['dist/**/*'],
+    },
+    {
+        files: ['js/**/*.js'],
+        rules: {
+            'no-const-assign': 'error',
+            'no-unused-vars': 'warn',
+            'no-undef': 'error',
+        },
+        languageOptions: {
+            globals: {
+                document: true,
+                console: true,
+                window: true,
+                require: true,
+                module: true,
+                exports: true,
+            },
+        },
+    },
 ];
